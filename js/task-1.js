7741a972c73cac3c848fdc84cf11e539ba72224e
vaskivstanislav <168464178+vaskivstanislav@users.noreply.github.com>
2024-08-02 21:35:19 +0300

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function slugify(title) {
+    const lowerCaseSlug = title.toLowerCase();
+    const slug = lowerCaseSlug.split(" ");
+    return slug.join("-")
+    
+}
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
---
+++ js/task-3.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function filterArray(numbers, value) {
+
+const fullArray = [];
+
+   for (const i of numbers) {
+      if (i > value) {
+       fullArray.push(i); 
+      }
+   }
+   return fullArray;
+ }
+
+ console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
